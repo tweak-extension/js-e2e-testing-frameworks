bf480c97e6b563c199eeda42a4802e17a86f90da
danielcaldas <caldasjdaniel@gmail.com>
2022-07-31 18:51:30 +0200

    chore: :city_sunrise:

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="content">
+        <p>Quotez</p>
+        <button onClick={() => {
+          setData(null)
+          fetch('/random-quote').then(r => r.json()).then(setData)
+        }}>
+          Hit me
+        </button>
+        {data ? <p className="quote">{`${data.quote} by ${data.author}`}</p> : <p>...</p>}
       </header>
     </div>
   );
